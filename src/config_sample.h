6cb1545b87e002796edee1e17cf41263b0999513
Max von Webel <max@343max.de>
2022-01-25 22:04:56 +0100

    seems to work

--- src/config_sample.h
+++ src/config_sample.h
@@ -1,3 +1,5 @@
 const char* ssid = "ssid";
 const char* password = "password";
 const char* hostname = "hostname";
+const char* ping_url_format = "http://host/boot?ip=%s&hostname=%s";
+const char* secret = "secret_string_change_me_really";
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,27 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include "asprintf.h"
 
 #include "config.h"
 
 int ledPin = LED_BUILTIN;
 WiFiServer server(80);
+WiFiClient wifiClient;
+HTTPClient sender;
+
+const int relayPin = D1;
+const long interval = 2000;
 
 void setup() {
   Serial.begin(9600);
   delay(10);
 
-
   pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin, HIGH);
+
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW);
 
   // Connect to WiFi network
   Serial.println();
@@ -32,6 +40,14 @@ void setup() {
   Serial.println("");
   Serial.println("WiFi connected");
 
+  char *url = NULL;
+  asprintf(&url, ping_url_format, WiFi.localIP().toString().c_str(), hostname);
+  Serial.println(url);
+  if (sender.begin(wifiClient, url)) {
+    sender.end();
+  }
+  free(url);
+
   // Start the server
   server.begin();
   Serial.println("Server started");
@@ -41,7 +57,6 @@ void setup() {
   Serial.print("http://");
   Serial.print(WiFi.localIP());
   Serial.println("/");
-
 }
 
 void loop() {
@@ -65,36 +80,24 @@ void loop() {
 
   // Match the request
 
-  int value = LOW;
-  if (request.indexOf("/LED=ON") != -1) {
+  if (request.indexOf(secret) == -1) {
+    Serial.println("invalid request");
+  } else {
+    Serial.println("buzzing");
     digitalWrite(ledPin, LOW);
-    value = LOW;
-  }
-  if (request.indexOf("/LED=OFF") != -1){
+    digitalWrite(relayPin, HIGH);
+    delay(400);
     digitalWrite(ledPin, HIGH);
-    value = HIGH;
+    digitalWrite(relayPin, LOW);
   }
-
-
 
   // Return the response
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
   client.println(""); //  do not forget this one
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>");
 
-  client.print("Led pin is now: ");
-
-  if(value == HIGH) {
-    client.print("On");
-  } else {
-    client.print("Off");
-  }
-  client.println("<br><br>");
-  client.println("Click <a href=\"/LED=ON\">here</a> turn the LED on pin 5 ON<br>");
-  client.println("Click <a href=\"/LED=OFF\">here</a> turn the LED on pin 5 OFF<br>");
-  client.println("</html>");
+  client.println("<!DOCTYPE html><body>Hello!</body></html>");
+  client.flush();
 
   delay(1);
   Serial.println("Client disconnected");
