417d34e7889ff8ce3a1cc8933e23d21d48814327
Max von Webel <max@343max.de>
2022-01-25 20:48:10 +0100

    moved secret stuff to config.h

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-const char* ssid = "343max";
-const char* password = "famukismus56";
-const char* hostname = "doorbuzzer";
+#include "config.h"
 
 int ledPin = LED_BUILTIN;
 WiFiServer server(80);
